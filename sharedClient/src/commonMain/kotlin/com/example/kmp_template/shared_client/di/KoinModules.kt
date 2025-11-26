0e24660e6c8311c4c0a950d16740d7b7983e7afb
FromWau <tobias.frommhund@aon.at>
2025-11-26 03:42:20 +0100

    fix

--- sharedClient/src/commonMain/kotlin/com/example/kmp_template/shared_client/di/KoinModules.kt
+++ sharedClient/src/commonMain/kotlin/com/example/kmp_template/shared_client/di/KoinModules.kt
@@ -4,10 +4,6 @@ import androidx.sqlite.driver.bundled.BundledSQLiteDriver
 import com.example.kmp_template.core.database.DatabaseFactory
 import com.example.kmp_template.shared_client.core.navigation.NavigationService
 import com.example.kmp_template.shared_client.core.presentation.toast.ToastService
-import com.example.kmp_template.shared_client.home.data.FeatureRepositoryImpl
-import com.example.kmp_template.shared_client.home.data.database.FeatureDatabase
-import com.example.kmp_template.shared_client.home.data.database.ModelDao
-import com.example.kmp_template.shared_client.home.domain.repository.FeatureRepository
 import com.example.kmp_template.shared_client.home.presentation.HomeViewModel
 import com.example.kmp_template.shared_client.settings.data.SettingsRepositoryImpl
 import com.example.kmp_template.shared_client.settings.data.database.SettingsDao
@@ -22,18 +18,6 @@ import org.koin.dsl.bind
 import org.koin.dsl.module
 
 val sharedModules = module {
-    // Example
-    single<FeatureDatabase> {
-        get<DatabaseFactory>().create<FeatureDatabase>(dbname = FeatureDatabase.DB_NAME)
-            .fallbackToDestructiveMigrationOnDowngrade(true)
-            .fallbackToDestructiveMigration(true)
-            .setDriver(BundledSQLiteDriver())
-            .setQueryCoroutineContext(Dispatchers.IO)
-            .build()
-    }
-    single<ModelDao> { get<FeatureDatabase>().modelDao }
-    singleOf(::FeatureRepositoryImpl) bind FeatureRepository::class
-
     // Navigation
     singleOf(::NavigationService)
 
